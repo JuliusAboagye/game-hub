e792d48298ceca7620a54cb7bfee1a320d727e44
Julius <juliusaboagye45@gmail.com>
2023-04-19 19:31:05 +0100

    Filter Games by platform

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -11,10 +11,10 @@ import {
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
-  onSelectgGenre: (genre: Genre) => void;
+  onSelectGenre: (genre: Genre) => void;
   selectedGenre: Genre | null;
 }
-const GenreList = ({ selectedGenre, onSelectgGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -33,7 +33,7 @@ const GenreList = ({ selectedGenre, onSelectgGenre }: Props) => {
               fontSize="lg"
               variant="link"
               fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-              onClick={() => onSelectgGenre(genre)}
+              onClick={() => onSelectGenre(genre)}
             >
               {genre.name}
             </Button>
